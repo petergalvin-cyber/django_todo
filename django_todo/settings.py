681df2d215e783e91ba8a4729d1f7d2a6255323b
petergalvin-cyber <petergalvin@consultant.com>
2020-03-09 16:13:13 +0000

    Settings fix

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 
-DATABASES = {'default':dj_database_url.parse("DATABASE_URL")}
+DATABASES = {'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
